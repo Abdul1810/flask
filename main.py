57e14e9ea229597b0761cf329b6f95bd81e7210f
AbdulRahman <61910010+Abdul1810@users.noreply.github.com>
2022-05-16 22:29:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template
+from flask_restful import Api, Resource
+
+from PIL import Image
+import base64
+import io
 import os
 
 app = Flask(__name__)
 
+img = Image.open("klaus.jpg")
+data = io.BytesIO()
+img.save(data, "JPEG")
+encoded_img_data = base64.b64encode(data.getvalue())
 
 @app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+def hello_world():
+    return render_template("index.html", img_data=encoded_img_data.decode('utf-8'))
 
+api = Api(app)
+
+class Hello(Resource):
+    def get(self):
+        return { "res": encoded_img_data.decode('utf-8') }, 200
+
+api.add_resource(Hello, '/api')
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
